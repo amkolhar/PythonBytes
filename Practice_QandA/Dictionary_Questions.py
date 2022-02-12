930def4a3786f2aecd86b91168d456005e673a3a
Atharv Kolhar <amkolhar@gmail.com>
2022-02-11 18:33:07 -0800

    Dictionary Question

---
+++ Practice_QandA/Dictionary_Questions.py
@@ -0,0 +1,16 @@
+# Atharv Kolhar
+# Python Bytes
+
+"""
+Fruits and their price
+market = {'Apple': 10,
+          'Banana': 5,
+          'Orange': 6,
+          'Grapes': 15}
+
+
+1. Added a new item to the dictionary Watermelon: 20.
+2. Print all the fruits available in the market
+3. Update the cost for Orange to 10
+"""
+

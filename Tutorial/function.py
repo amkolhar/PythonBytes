a0923a9a0ce8c495389dd2547a64d64c1e7a36ac
Atharv Kolhar <amkolhar@gmail.com>
2022-02-15 20:33:30 -0800

    Function lecture

---
+++ Tutorial/function.py
@@ -0,0 +1,40 @@
+# Atharv Kolhar
+# Python Bytes
+
+"""
+Functions
+"""
+
+# Definition of Function
+def add(a:int, b:int):
+
+    total = a+ b
+
+    return total
+
+
+def subtract(a:int, b:int):
+
+    diff = a - b
+
+    return diff
+
+# Use of function
+c = 10
+d = 8
+
+total = add(c, d)
+diff = subtract(c, d)
+
+print(total)
+print(diff)
+
+e = 30
+f = 20
+
+ef_total = add(e, f)
+ef_diff = subtract(e, f)
+
+print(ef_total, ef_diff)
+
+# End
